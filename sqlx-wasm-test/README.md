80f60cb664986ef370c6c57ffd3d60606c681664
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-04-26 21:35:18 +0530

    Add support for benchmarking updates

--- sqlx-wasm-test/README.md
+++ sqlx-wasm-test/README.md
@@ -1,5 +1,5 @@
 # Setup
-1. Make sure postgres is installed and listening at the 5432 port.
+1. Make sure postgres is installed and listening at port 5432.
 2. Start a websocket-tcp proxy using [websocat](https://github.com/vi/websocat)
    `$ websocat --binary ws-l:127.0.0.1:8080 tcp:127.0.0.1:5432`
 
--- sqlx-wasm-test/src/lib.rs
+++ sqlx-wasm-test/src/lib.rs
@@ -36,7 +36,7 @@ macro_rules! time_query {
 macro_rules! time_insert_query {
     ($n:expr, $count:literal) => {
         let mut conn = sqlx_wasm_test::new().await;
-        conn.execute("create temp table bench_inserts (id integer, desc text)")
+        conn.execute("create temp table bench_inserts (id integer, descr text)")
             .await;
 
         let performance = web_sys::window().unwrap().performance().unwrap();
@@ -46,6 +46,32 @@ macro_rules! time_insert_query {
             for i in 0..$count {
                 let _ = sqlx::query(&format!(
                     "insert into bench_inserts (id, desc) values ({}, md5(random()::text))",
+                    i
+                ))
+                .execute(&mut conn)
+                .await;
+            }
+        }
+
+        let end = performance.now();
+        web_sys::console::log_1(&format!("{}: Avg time is {}", $n, (end - start) / 3f64).into());
+    };
+}
+
+#[macro_export]
+macro_rules! time_update_query {
+    ($n:expr, $count:literal) => {
+        let mut conn = sqlx_wasm_test::new().await;
+        conn.execute("create temp table bench_updates as SELECT generate_series(1,100) AS id, md5(random()::text) AS descr ")
+            .await;
+
+        let performance = web_sys::window().unwrap().performance().unwrap();
+        let start = performance.now();
+
+        for _ in 0..3u8 {
+            for i in 1..$count {
+                let _ = sqlx::query(&format!(
+                    "update bench_updates set descr = md5(random()::text) where id = {}",
                     i
                 ))
                 .execute(&mut conn)
