a987698d4b06a6a2e8012cceaa05253f433c1eb5
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-05-08 10:01:08 +0530

    WIP: add time tests

--- sqlx-wasm-test/src/pg_wasm_tests.rs
+++ sqlx-wasm-test/src/pg_wasm_tests.rs
@@ -289,10 +289,6 @@ async fn it_supports_domain_types_in_composite_domain_types() {
     }
 }
 
-<<<<<<< Updated upstream:sqlx-wasm-test/src/pg_wasm_tests.rs
-=======
-#[cfg(feature = "json")]
->>>>>>> Stashed changes:sqlx-wasm-test/src/selects_tests.rs
 #[wasm_bindgen_test]
 async fn it_describes_and_inserts_json_and_jsonb() {
     let mut conn = new().await;
@@ -317,7 +313,6 @@ async fn it_describes_and_inserts_json_and_jsonb() {
 
     assert_eq!(done.rows_affected(), 1);
 }
-<<<<<<< Updated upstream:sqlx-wasm-test/src/pg_wasm_tests.rs
 
 #[wasm_bindgen_test]
 async fn test_listener_cleanup() {
@@ -336,5 +331,3 @@ async fn test_listener_cleanup() {
         "Notification sent and received"
     );
 }
-=======
->>>>>>> Stashed changes:sqlx-wasm-test/src/selects_tests.rs
